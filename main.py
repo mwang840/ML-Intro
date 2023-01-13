8ea38b9541448775a34d598513922ab782a3f184
mwang840 <maxwang@udel.edu>
2023-01-12 21:48:01 -0500

    More ML concepts

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+import matplotlib
+#import numpy as np
+import matplotlib.pyplot as plt
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    x = [2, 4, 6, 8, 10]
+    y = [6, 12, 18, 24, 30]
+    plt.plot(x, y)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
